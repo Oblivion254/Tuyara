976c13bb7c4cfb8710dfedf3a5af08e4e12530a1
Vignesh M <vickyaadhi254@gmail.com>
2024-09-01 19:28:21 +0530

    Add: requirments.txt

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def emotion():
                 return render_template("leave_feedback.html") 
   
 if __name__ == '__main__':  
-    app.run(debug = True)  
+    app.run(debug = True, host="0.0.0.0",port=5000)  
